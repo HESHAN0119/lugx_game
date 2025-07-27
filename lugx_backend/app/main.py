f31f94235dd45a3b95d7e38e7e2fa4a2d1631a62
HESHAN0119 <heshanravindu1999@gmail.com>
2025-07-27 23:07:58 +0530

    test commit

--- lugx_backend/app/main.py
+++ lugx_backend/app/main.py
@@ -20,8 +20,7 @@ from clickhouse_driver.errors import NetworkError
 from datetime import datetime
 from fastapi import HTTPException, Request
 
-
-
+#test
 logging.basicConfig(level=logging.INFO)
 logger=logging.getLogger(__name__)
 
